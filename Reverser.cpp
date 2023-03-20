c84de8b3fb7b92b3deb60987a80a8b27b265863f
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-03-20 22:26:53 +1030

    Add files via upload

--- Reverser.cpp
+++ Reverser.cpp
@@ -3,10 +3,11 @@
 
 int Reverser::reverseDigit(int value)
 {
-    int rev_num = 0;
-    while (value > 0) {
-        rev_num = rev_num * 10 + value % 10;
+    //int rev_num = 0;
+    if (value > 0) {
+        rev_num = rev_num*10 + value % 10;
         value = value / 10;
+		return reverseDigit(value);
     }
     return rev_num;
 }
