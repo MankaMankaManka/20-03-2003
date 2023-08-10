c424abf3709077649ebde0687372884fe77793ba
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-08-10 11:20:33 +0930

    Update Reverser.h

--- Reverser.h
+++ Reverser.h
@@ -5,7 +5,7 @@
 class Reverser {       // The class
   public:             // Access specifier
 	int reverseDigit(int value);
-    int value;
+    	int value=0;
 	int newNum=0;
 	Reverser();
 	std::string reverseString(std::string characters);
